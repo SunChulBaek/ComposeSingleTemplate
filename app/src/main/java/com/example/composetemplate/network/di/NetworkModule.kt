140e0606a0c9773426ad5c1c6c6fcdfe1a049236
x1210x <x1210x@gmail.com>
2024-01-04 17:56:24 +0900

    libraries updates

--- app/src/main/java/com/example/composetemplate/network/di/NetworkModule.kt
+++ app/src/main/java/com/example/composetemplate/network/di/NetworkModule.kt
@@ -11,5 +11,7 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 interface SsunNetworkModule {
     @Binds
-    fun KtorSsunNetwork.binds(): SsunNetworkDataSource
+    fun bindKtorSsunNetwork(
+        network: KtorSsunNetwork
+    ): SsunNetworkDataSource
 }
